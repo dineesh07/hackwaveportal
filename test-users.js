6ce4c2c3d4f504363389d15fd6e28eb969c15ad7
dineesh07 <dineeshkalai07@gmail.com>
2026-08-12 22:13:15 +0530

    Remove debug files that break Vercel build

--- test-users.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.user.findMany().then(u => {
-    console.log("Total users:", u.length);
-    if(u.length > 0) {
-        console.log("First 3 users:", JSON.stringify(u.slice(0, 3).map(x=>({rollNo:x.rollNo, status:x.status})), null, 2));
-    }
-}).catch(console.error).finally(() => prisma.$disconnect());
